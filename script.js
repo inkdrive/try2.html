32549bec9b555f5dfee5028ec7427ff43d77e6a2
inkdrive <fractal.ink.drive@gmail.com>
2024-12-30 07:55:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// env.js
-const SUPABASE_URL = 'https://khhpbnpdtkvymrycvtgi.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoaHBibnBkdGt2eW1yeWN2dGdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyODQzNzAsImV4cCI6MjA1MDg2MDM3MH0.RGTy42QZUyb-cdmtWnzLa199u4N20kLEaVmC9yoPtus';
+
 // js/script.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
