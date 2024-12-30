344dc0d699cb8044a763df9042b25ddcec55caec
inkdrive <fractal.ink.drive@gmail.com>
2024-12-30 07:50:51 +0530

    Add files via upload

---
+++ env.js
@@ -0,0 +1,3 @@
+// env.js
+const SUPABASE_URL = 'https://khhpbnpdtkvymrycvtgi.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoaHBibnBkdGt2eW1yeWN2dGdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyODQzNzAsImV4cCI6MjA1MDg2MDM3MH0.RGTy42QZUyb-cdmtWnzLa199u4N20kLEaVmC9yoPtus';
